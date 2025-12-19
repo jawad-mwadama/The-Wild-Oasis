103519396088c622c9b7411aa57de9e53044cdcb
jawad-mwadama <allymwadama@gmail.com>
2025-12-19 20:40:05 +0300

    returned staletime to origina; time

--- src/App.jsx
+++ src/App.jsx
@@ -17,8 +17,8 @@ import AppLayout from "./ui/AppLayout";
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
-      // staleTime: 60 * 1000,
-      staleTime: 0,
+      staleTime: 60 * 1000,
+      // staleTime: 0,
     },
   },
 });
