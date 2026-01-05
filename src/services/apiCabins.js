bfdd25e7d86a53fdf8a230a465a32a9e09620346
jawad-mwadama <allymwadama@gmail.com>
2026-01-05 22:44:29 +0300

    fixed image upload issue

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -15,7 +15,7 @@ export async function createCabin(newCabin) {
     ""
   );
 
-  const imagePath = `${supabaseUrl}storage/v1/object/public/cabin-images/${imageName}`;
+  const imagePath = `${supabaseUrl}/storage/v1/object/public/cabin-images/${imageName}`;
 
   // 1. Create Cabin
   const { data, error } = await supabase
